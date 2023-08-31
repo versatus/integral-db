cf9e994ff55e9edf3a2e0991bdae855ad1801f94
Chris-san <57543709+eureka-cpu@users.noreply.github.com>
2023-08-31 12:50:30 -0500

    Auto increment version for `lr_trie` (#10)
    * Auto increment version for lr_trie and wrapper

--- lr_trie/src/absorb_op.rs
+++ lr_trie/src/absorb_op.rs
@@ -1,3 +1,4 @@
+use crate::Operation;
 use left_right::Absorb;
 pub use left_right::ReadHandleFactory;
 use patriecia::{
@@ -5,7 +6,8 @@ use patriecia::{
 };
 use tracing::error;
 
-use crate::Operation;
+/// The number by which the [`Version`] of a [`JellyfishMerkleTree`] is incremented.
+const INCREMENT_ARG: u64 = 1;
 
 impl<D, H> Absorb<Operation> for JellyfishMerkleTree<D, H>
 where
@@ -13,10 +15,17 @@ where
     H: SimpleHasher,
 {
     fn absorb_first(&mut self, operation: &mut Operation, _other: &Self) {
+        // TODO: the unwrap_or_default avoids panic but is not logically sound and should
+        // not be used in production since `Version` is monotonically increasing and could
+        // eventually overflow.
+        //
+        // cc @nopestack
+        let increment_version =
+            |vers: &mut u64| vers.checked_add(INCREMENT_ARG).unwrap_or_default();
         match operation {
             // TODO: report errors via instrumentation
             Operation::Add(key_val, vers) => {
-                match self.put_value_set(vec![key_val.to_owned()], *vers) {
+                match self.put_value_set(vec![key_val.to_owned()], increment_version(vers)) {
                     Ok((_, batch)) => {
                         if let Err(err) = self.reader().write_node_batch(&batch.node_batch) {
                             error!("Operation::Add failed to write changes to database: {err}")
@@ -25,22 +34,26 @@ where
                     Err(err) => error!("Operation::Add failed to insert key: {err}"),
                 }
             }
-            Operation::Remove(key, vers) => match self.put_value_set(vec![(*key, None)], *vers) {
-                Ok((_, batch)) => {
-                    if let Err(err) = self.reader().write_node_batch(&batch.node_batch) {
-                        error!("Operation::Remove failed to write changes to database: {err}")
+            Operation::Remove(key, vers) => {
+                match self.put_value_set(vec![(*key, None)], increment_version(vers)) {
+                    Ok((_, batch)) => {
+                        if let Err(err) = self.reader().write_node_batch(&batch.node_batch) {
+                            error!("Operation::Remove failed to write changes to database: {err}")
+                        }
                     }
+                    Err(err) => error!("Operation::Remove failed to remove value for key: {err}"),
                 }
-                Err(err) => error!("Operation::Remove failed to remove value for key: {err}"),
-            },
-            Operation::Extend(kvs, vers) => match self.put_value_set(kvs.to_vec(), *vers) {
-                Ok((_, batch)) => {
-                    if let Err(err) = self.reader().write_node_batch(&batch.node_batch) {
-                        error!("Operation::Extend failed to write changes to database: {err}")
+            }
+            Operation::Extend(kvs, vers) => {
+                match self.put_value_set(kvs.to_vec(), increment_version(vers)) {
+                    Ok((_, batch)) => {
+                        if let Err(err) = self.reader().write_node_batch(&batch.node_batch) {
+                            error!("Operation::Extend failed to write changes to database: {err}")
+                        }
                     }
+                    Err(err) => error!("Operation::Extend failed batch update: {err}"),
                 }
-                Err(err) => error!("Operation::Extend failed batch update: {err}"),
-            },
+            }
         }
     }
 
